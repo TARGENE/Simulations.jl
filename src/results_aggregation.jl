b3ae846b55c7e5861c94f6ebb4887ebebb32c382
Olivier Labayle <olabayle@gmail.com>
2024-07-30 12:05:14 +0100

    add nan fix

--- src/results_aggregation.jl
+++ src/results_aggregation.jl
@@ -37,7 +37,10 @@ function covers(Ψ̂, Ψ₀; alpha=0.05)
     return pval === NaN ? NaN : pval > alpha
 end
 
-mean_coverage(estimates, Ψ₀) = mean(skipnan(covers(Ψ̂, Ψ₀) for Ψ̂ in estimates))
+"""
+The `collect` function is used so that this function returns NaN if all coverage measures are NaNs
+"""
+mean_coverage(estimates, Ψ₀) = mean(collect(skipnan(covers(Ψ̂, Ψ₀) for Ψ̂ in estimates)))
 
 add_mean_coverage_col!(results_df) = 
     results_df.MEAN_COVERAGE = [mean_coverage(Ψ̂s, Ψ₀) for (Ψ̂s, Ψ₀) in zip(results_df.ESTIMATES, results_df.TRUE_EFFECT)]
